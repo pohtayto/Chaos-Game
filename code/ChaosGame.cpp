4b18634566c885b2882809bbb8a4cebfa237d593
Your Name <you@example.com>
2023-11-09 22:54:02 -0800

    text and font

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -82,6 +82,26 @@ int main()
 		Draw
 		****************************************
 		*/
+        sf::Font font;
+        sf::Text text;
+
+        // select the font
+        text.setFont(font); // font is a sf::Font
+
+        // set the string to display
+        text.setString("Hello world");
+
+        // set the character size
+        text.setCharacterSize(24); // in pixels, not points!
+
+        // set the color
+        text.setFillColor(sf::Color::Red);
+
+        // set the text style
+        text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+
+        // inside the main loop, between window.clear() and window.display()
+        window.draw(text);
         window.clear();
         for(int i = 0; i < vertices.size(); i++)
         {
